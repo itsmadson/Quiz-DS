43bd0947942c2858b07b446eff7efb6ef4f5169a
Madson <msemsemse4545s@gmail.com>
2025-04-12 21:16:05 +0330

    4

--- 4.py
+++ 4.py
@@ -1,5 +1,6 @@
 def recursive_sum(lst):
-    return lst[0] + recursive_sum(lst[1:])
+    if len(lst) == 0: # if list is empty 
+        return 0
+    return lst[0] + recursive_sum(lst[1:]) # first element + other element => X+(X+1...)
 
-
-print(recursive_sum([1, 2, 3, 4]))  
+print(recursive_sum([1, 2, 3, 4])) # result : 10
